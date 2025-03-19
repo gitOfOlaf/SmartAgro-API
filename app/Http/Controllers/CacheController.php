dc2c40fbbad11d9cef40518950e1d5b30122eb90
olafer <olafer25227@gmail.com>
2025-03-19 17:22:56 -0300

    cache

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -10,9 +10,10 @@ class CacheController extends Controller
     public function clearCache()
     {
         try {
-            Artisan::call('config:clear');
-            Artisan::call('cache:clear');
             Artisan::call('route:clear');
+            Artisan::call('cache:clear');
+            Artisan::call('config:clear');
+            Artisan::call('config:cache');
             Artisan::call('view:clear');
             
             return response()->json(['message' => 'Cache limpiada correctamente'], 200);
